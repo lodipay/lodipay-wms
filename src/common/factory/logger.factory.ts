8572dd9303d039554ee78ffb02499ca1f1d8f749
Batbayar Tutaa <batbayar0105@gmail.com>
2023-05-04 13:48:30 +0800

    Log setup done

---
+++ src/common/factory/logger.factory.ts
@@ -0,0 +1,14 @@
+import 'winston-daily-rotate-file';
+import { WinstonFactory } from './logger/winston.factory';
+
+const registeredFactories = [WinstonFactory];
+
+export class LoggerFactory {
+    static create(transport: string, options: object) {
+        for (const factory of registeredFactories) {
+            if (factory.supports(transport)) {
+                return factory.create(transport, options);
+            }
+        }
+    }
+}
---
+++ src/common/factory/logger.interface.ts
@@ -0,0 +1,6 @@
+import { LogLevel, LoggerService } from '@nestjs/common';
+
+export interface LoggerInterface {
+    create(transport: string, options): LoggerService | boolean | LogLevel;
+    supports(transport: string): boolean;
+}
