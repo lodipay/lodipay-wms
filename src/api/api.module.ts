008078a1b97148b3327cc036f2ae0a27210d72f2
batbayar <batbayar0105@gmail.com>
2023-05-09 14:40:34 +0800

    fix(api): linted lock crud

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LocationModule } from './location/location.module';
-import { WarehouseModule } from './warehouse/warehouse.module';
 import { LockModule } from './lock/lock.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
 
 @Module({
   imports: [WarehouseModule, LocationModule, LockModule],
--- src/database/migrations/Migration20230509063032.ts
+++ src/database/migrations/Migration20230509063032.ts
@@ -1,7 +1,6 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20230509063032 extends Migration {
-
   async up(): Promise<void> {
     this.addSql('create table "lock" ("id" serial primary key, "reason" varchar(255) not null, "from" timestamptz(0) null, "to" timestamptz(0) null);');
   }
@@ -9,5 +8,4 @@ export class Migration20230509063032 extends Migration {
   async down(): Promise<void> {
     this.addSql('drop table if exists "lock" cascade;');
   }
-
 }
