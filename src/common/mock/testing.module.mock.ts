cda52f6708e4aebf7b8f1d321c23443c489abe95
batbayar <batbayar0105@gmail.com>
2023-05-15 14:11:57 +0800

    fix(test): changed mikro orm test connection to sync

--- src/common/mock/testing.module.mock.ts
+++ src/common/mock/testing.module.mock.ts
@@ -1,32 +1,20 @@
-import {
-  MikroOrmModule,
-  MikroOrmModuleOptions,
-  MikroOrmOptionsFactory,
-} from '@mikro-orm/nestjs';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { Test, TestingModule } from '@nestjs/testing';
-
-export default class TestDatabaseConfig implements MikroOrmOptionsFactory {
-  createMikroOrmOptions(): MikroOrmModuleOptions {
-    return {
-      name: 'default',
-      type: 'postgresql',
-      debug: false,
-      autoLoadEntities: false,
-      connect: false,
-      dbName: 'test',
-      entities: ['dist/src/**/*.entity.{js,ts}'],
-      entitiesTs: ['src/**/*.entity.ts'],
-      metadataProvider: TsMorphMetadataProvider,
-    };
-  }
-}
 
 export const getTestingModule = (providers: any[]): Promise<TestingModule> => {
   return Test.createTestingModule({
     imports: [
-      MikroOrmModule.forRootAsync({
-        useClass: TestDatabaseConfig,
+      MikroOrmModule.forRoot({
+        name: 'default',
+        type: 'postgresql',
+        debug: false,
+        autoLoadEntities: false,
+        connect: false,
+        dbName: 'test',
+        entities: ['dist/src/**/*.entity.{js,ts}'],
+        entitiesTs: ['src/**/*.entity.ts'],
+        metadataProvider: TsMorphMetadataProvider,
       }),
     ],
     providers,
