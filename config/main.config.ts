d83f481209003444074582e84d46c3415c3a6aba
badamgarav@lodipay.mn <badamgarav@lodipay.mn>
2024-05-29 17:04:27 +0800

    fix: api prefix

--- config/main.config.ts
+++ config/main.config.ts
@@ -4,6 +4,7 @@ export class MainConfig {
     inDevelopment: boolean;
     port: number;
     apiDomain: string;
+    apiPrefix: string;
 }
 
 export const CONFIG_NAME_MAIN = 'MAIN_CONFIG';
@@ -12,4 +13,5 @@ export default registerAs(CONFIG_NAME_MAIN, () => ({
     inDevelopment: process.env.ENVIRONMENT === process.env.ENVIRONMENT_DEV,
     port: process.env.WHS_PORT || 13000,
     apiDomain: process.env.API_URL,
+    apiPrefix: process.env.API_PREFIX,
 }));
